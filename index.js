7455757c9f784dfd5be56b152a6ffe76860fb9b8
MariaBarakhova <37710370+MariaBarakhova@users.noreply.github.com>
2018-07-20 21:24:31 +0300

    111

--- index.js
+++ index.js
@@ -1,22 +1,227 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => {
-    console.log(props);
-    return(
-        <div className='header' >
-        <h1>Header</h1>
-        <nav className='menu'>
+import SearInput from '../../components/SearchInput';
+import UsersList from '../../components/UsersList';
+import ActiveUser from '../../components/ActiveUser';
+
+import data from '../../data.txt';
+import logo from '../../logo.svg';
+import '../../App.css';
+import ItemUser from '../../components/ItemUser';
+
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      activeUser: null,
+      currentPage: 0,
+      listPages: null,
+      isShowContent: false,
+      isErrorUser: false,
+    };
+  }
+
+
+  componentWillMount() {
+    fetch(data)
+    .then(response => response.json())
+    .then(data => {
+      this.initialData = data;
+      this.setState({
+        defaultUsers: [...data],
+        data: this.initialData,
+        activeUser: data[0],
+      });
+    })
+  }
+
+  updateApp(config) {
+    this.setState(config);
+    if(config.activeUser.id === this.state.activeUser.id) {
+      this.setState({
+        isErrorUser: true,
+      })
+    }
+    else{
+      this.setState({
+        isErrorUser: false,
+      })
+    }
+    console.log(config);
+  }
+
+  toggleContent = () => {
+    this.setState({
+      isShowContent: !this.state.isShowContent
+    });
+  }
+
+  search = (e) => {
+    console.log(e.target);
+    const value = e.target.value.toLowerCase();
+    const fillter = this.initialData.filter(user => {
+      return user.name.toLowerCase().includes(value);
+    });
+    this.updateApp({
+      data: fillter,
+      currentPage: 0,
+    });
+  }
+
+  sort = (type) =>{
+    const { data } = this.state;
+    const dataUsers = data;
+    const sorted = dataUsers.sort((a, b)=> {
+      return a[type] > b[type] ? 1 : -1;
+    });
+this.setState({
+  data: sorted,
+})
+    console.log('sorted', sorted);
+  }
+  reset = () => {
+    this.updateApp({
+      data: this.state.defaultUsers,
+      activeUser: this.state.defaultUsers[0],
+    });
+  }
+  
+  splitUsers = () => {
+    const {
+      data,
+      currentPage,
+    } = this.state;
+    return data && data.slice(currentPage * 15, currentPage * 15 + 15);
+  }
+
+  
+  handlePagitaion = (number) => {
+    const { currentPage, data } = this.state;
+    const listPages = Math.ceil(data.length / 15);
+    if(number + currentPage >= 0 && number + currentPage < listPages) {
+      this.setState(prev => (console.log(prev),{
+        listPages,
+        currentPage: prev.currentPage + number,
+      }))
+    }
+  }
+
+  render() {
+    console.log('listPages',this.state.listPages);
+    console.log('currentPage',this.state.currentPage);
+    const buttonsList = [
+      {
+        name: 'Name',
+        icon: 'fa-users',
+        onClick: this.test,
+        onClick: () => this.sort('name'),
+      },
+      {
+        name: 'Age',
+        icon: 'fa-ban',
+        onClick: () => this.sort('age'),
+      },
+      {
+        name: 'Reset',
+        icon: 'fa-ban',
+        onClick:() => console.log('222'),
+      },
+    ];
+
+
+    const test1 = [
+      {
+        name: 'Ann',
+        age: '25',
+        
+      },
+      {
+        name: 'Jane',
+        age: '26',
+        
+      },
+    ];
+
+
+    const {
+      data,
+      listPages,
+      currentPage,
+      isErrorUser,
+    } = this.state;
+    return (
+      <div className='home'>
+       <header className={`App-header ${this.state.isHeaderClass ? 'red' : ''}`}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+
+          <button className='btn btn-primary'  
+           onClick={this.toggleContent}>
+            button
+          </button>
+
         {
-            props.menuList &&  props.menuList.map(item =>
-            <button>
-                {
-                    item.title
-                }
-            </button>
+          this.state.isShowContent && test1.map(item => {
+         return (<div>
+              {item.name} {item.age}
+          </div>
             )
+          })
         }
 
-        </nav>
+
+
+        <div className="home__header">
+
+          <SearInput
+            isError={data && data.length === 0}
+            searchValue={this.search}
+          />
+          <div className='rowSorting'>
+            {
+              buttonsList.map((item, id) =>
+              <button
+                key={id}
+                type='button'
+                onClick={item.onClick}
+                className='btn btn-primary'
+              >
+                <i className={`fa ${item.icon}`}></i>
+                <p className='btnName'>
+                  {item.name}
+                </p>
+              </button>
+            )
+            }
+          </div>
         </div>
+        <div className="home__content">
+          <div className="home__sidebar">
+            <div className='sidebar'>
+              <ActiveUser
+              isError={isErrorUser}
+                user={this.state.activeUser}
+              />
+            </div>
+          </div>
+          <div className="home__wrapUsers">
+            <div className="usersHeader">
+              <h2 className='usersHeader__title'>Users</h2>
+            </div>
+            <UsersList
+              handlePagitaion={this.handlePagitaion}
+              currentPage={currentPage}
+              listPages={listPages}
+              data={this.splitUsers()}
+              updateApp={this.updateApp.bind(this)}
+            />
+          </div>
+        </div>
+      </div>
     );
+  }
 }
+
